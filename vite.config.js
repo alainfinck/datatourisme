221be9f910e67643298edea58683ad8fe8ab4d43
Alain Finck <alain@wallprint.fr>
2026-03-02 13:00:46 +0100

    fix: add vite proxy for local development

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/health': 'http://localhost:3001',
+      '/contacts.json': 'http://localhost:3001',
+      '/datatourisme.csv': 'http://localhost:3001',
+      '/socket.io': {
+        target: 'http://localhost:3001',
+        ws: true,
+      },
+    },
+  },
 })
